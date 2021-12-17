0a423727341cbf804da1923fecb38dd4511fb7f3
akhilanandsirra <akhilanand9999@gmail.com>
2021-12-17 15:34:51 -0500

    code done

--- src/main/java/LargestTree.java
+++ src/main/java/LargestTree.java
@@ -1,23 +1,35 @@
 import java.util.*;
+
 class LargestTree{
     static void addEdge(LinkedList<Integer> adj[], int u, int v)
     {
-        //TO-DO:
+        adj[u].add(v);
+        adj[v].add(u);
     }
 
     static int DFS(int u, LinkedList<Integer> adj[], Vector<Boolean> visited)
     {
         visited.add(u, true);
         int size = 1;
-
-        // Iterate through all the nodes and perform DFS if the node is not yet visited
-        //TO-DO:
-        return 0;
+        for (int i = 0; i < adj[u].size(); i++)
+            if (!visited.get(adj[u].get(i)))
+                size += DFS(adj[u].get(i),adj, visited);
+        return size;
     }
 
     public int largestTree(LinkedList<Integer> adj[], int V)
     {
-        //TO-DO:
-        return 0;
+        int largestTree = 0;
+        Vector<Boolean> visited = new Vector<>();
+        for(int i = 0; i < V; i++){
+            visited.add(false);
+        }
+        for (int u = 0; u < V; u++)
+        {
+            if (!visited.get(u)) {
+                largestTree = Math.max(largestTree, DFS(u, adj, visited));
+            }
+        }
+        return largestTree;
     }
 }
